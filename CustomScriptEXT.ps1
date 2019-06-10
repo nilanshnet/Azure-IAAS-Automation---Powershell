4ece2fc6a625fae1176c3d887f9bf8c3a0cf4bad
Nilansh Netan <nil.netan963@gmail.com>
2019-06-10 14:16:41 +0530

    Update CustomScriptEXT.ps1

--- CustomScriptEXT.ps1
+++ CustomScriptEXT.ps1
@@ -1,13 +1,16 @@
-﻿
-$stgacc = Get-AzureRmStorageAccount -ResourceGroupName "POC" -Name "pocstgacc001" 
-$stgacc | Get-AzureStorageContainer -Name "srf" | Set-AzureStorageContainerAcl -Permission Blob
+$stgaccname  # existing storage account
+$containerName # name of the container within the Storage account
+$rgname # name of the storage account
+
+$stgacc = Get-AzureRmStorageAccount -ResourceGroupName $rgname -Name $stgaccname 
+$stgacc | Get-AzureStorageContainer -Name $containerName | Set-AzureStorageContainerAcl -Permission Blob
 
 $stgaccname = $stgacc.StorageAccountName
 $rgname = $stgacc.ResourceGroupName
 $filename = @("SRF.ps1")
 
-Set-AzureRmVMCustomScriptExtension -ContainerName "srf" -Location "eastus" -FileName $filename `
--StorageAccountName "pocstgacc001" -ResourceGroupName "poc" -VMName "arvdrjmp001" -Name "srf"
+Set-AzureRmVMCustomScriptExtension -ContainerName $containerName -Location "eastus" -FileName $filename `
+-StorageAccountName $stgaccname -ResourceGroupName $rgname -VMName $vmname -Name $VMextensionName
 
 <#
 # code for azure devops
