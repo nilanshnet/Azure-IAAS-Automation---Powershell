9edd0e110a1674d6c43e0f84abe677d480fd51f1
Nilansh Netan <nil.netan963@gmail.com>
2019-06-10 14:25:30 +0530

    Update ResizeDISk.ps1

--- ResizeDISk.ps1
+++ ResizeDISk.ps1
@@ -1,20 +1,22 @@
-﻿
-$rgName = 'GAV-ARV-DR-DEM-01'
-$diskName = ''
+$rgName = 'DEM-01'
+$diskName = 'disk001'
 
+# Get the managed Disk using the Name  
 $disk = Get-AzureRmDisk -DiskName $diskName -ResourceGroupName $rgName
 
+# get the Virtual machine using the Disk
 $vmResource = Get-AzureRmResource -ResourceId $disk.ManagedBy
 
+# Stopping the VM
 Stop-AzureRmVM -ResourceGroupName $vmResource.ResourceGroupName -Name $vmResource.Name -Force
 
 $disk.DiskSizeGB = 258
 
-Start-Sleep -s 15
+# sleep time of 15 seconds so that the VM is stopped before proceeding
+Start-Sleep -s 15   
 
+# update the disk configuration
 Update-AzureRMDisk -ResourceGroupName $rgName -Disk $disk -DiskName $disk.Name
 
+# starting the VM again
 Start-AzureRmVM -ResourceGroupName $vmResource.ResourceGroupName -Name $vmResource.Name
-
-
-
