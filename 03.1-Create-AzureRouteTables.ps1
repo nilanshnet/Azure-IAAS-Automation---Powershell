806d790e7280f0feaabb858c7348cf6427bde03a
Nilansh Netan <nil.netan963@gmail.com>
2020-06-25 16:13:14 +0530

    Update and rename 03.1-Create-AzurermRouteTables.ps1 to 03.1-Create-AzureRouteTables.ps1

--- 03.1-Create-AzureRouteTables.ps1
+++ 03.1-Create-AzureRouteTables.ps1
@@ -2,19 +2,19 @@
 .NOTES 
     File Name  : New-UDR.ps1
                :
-    Author     : Charles Long - charlesl@hanu.com
+    Author     : Nilansh Netan - nil.netan963@gmail.com
                :
-    Created    : 04/16/2019 v1.0
-	Updated	   : MM/DD/2017 v1.1
+    Created    : 04/16/2020 v1.0
+
                  
                  
 #>
 
-Select-AzureRmSubscription -SubscriptionName "HCAUK - Division"
+Select-AzureRmSubscription -SubscriptionName ""
 
-$rg = 'hcauks-prod-hub01-rg'
-$vnetname = 'hcauks-prod-hub01-vnet'
-$loc = 'UK South'
+$rg = 'prod-hub01-rg'
+$vnetname = 'prod-hub01-vnet'
+$loc = 'East US'
 
 
 $Route = New-AzureRmRouteConfig -name "default.route" -AddressPrefix "0.0.0.0/0" -NextHopType VirtualAppliance -NextHopIpAddress "10.120.16.121"
